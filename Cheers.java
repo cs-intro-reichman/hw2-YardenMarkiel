d3aed9588950bdf8f8ff2a4fc5827b716c2b1d24
YardenMarkiel <yarden.markiel@runi.co.il>
2024-11-17 15:44:03 +0200

    fixed 4

--- Cheers.java
+++ Cheers.java
@@ -25,14 +25,14 @@ public class Cheers {
                         for (int i = 0; i < 11; i++) { //checks if we vhave special letter
                                 if(fixedS.charAt(checkLen) == anChar.charAt(x)){
                                         chant = chant + fixedS.charAt(checkLen);
-                                        System.out.println("Give me an " + fixedS.charAt(checkLen) + ": " + fixedS.charAt(checkLen) +"!");
+                                        System.out.println("Give me an " + fixedS.charAt(checkLen) + ":  " + fixedS.charAt(checkLen) +"!");
                                         checkAn++;                           
                                 }
                                 x++;
                         }
                         if(checkAn == 0){ //if we dont have special 
                                 chant = chant + fixedS.charAt(checkLen);
-                                System.out.println("Give me a " + fixedS.charAt(checkLen) + ": " + fixedS.charAt(checkLen) + "!");
+                                System.out.println("Give me a " + fixedS.charAt(checkLen) + ":  " + fixedS.charAt(checkLen) + "!");
                         }
                         checkAn = 0;
                         checkLen++;
